b9de31687e836da6adb1cb3f16ab4ed715e7ca92
Kateryna Kocheharova <kot41901@gmail.com>
2024-06-05 19:47:41 +0300

    fix errors

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -4,7 +4,7 @@ import axios from "axios";
 axios.defaults.baseURL = "https://666065685425580055b3b5cc.mockapi.io/contacts";
 
 export const fetchContacts = createAsyncThunk(
-  "tasks/fetchAll",
+  "contacts/fetchAll",
   async (_, thunkAPI) => {
     try {
       const res = await axios.get("");
@@ -16,14 +16,13 @@ export const fetchContacts = createAsyncThunk(
 );
 
 export const addContact = createAsyncThunk(
-  "tasks/addContact",
+  "contacts/addContact",
   async (values, thunkAPI) => {
     try {
       const res = await axios.post("", {
         name: values.name,
         number: values.number,
       });
-      console.log(res.data);
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -32,11 +31,10 @@ export const addContact = createAsyncThunk(
 );
 
 export const deleteContact = createAsyncThunk(
-  "tasks/deleteContact",
+  "contacts/deleteContact",
   async (id, thunkAPI) => {
     try {
       const res = await axios.delete(`${id}`);
-      console.log(res.data);
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
